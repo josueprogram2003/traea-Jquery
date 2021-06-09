81bc5ff72caa4eda2adbd4ac806e03acb9b5dba1
Jdominguez <josuedominguezonline12@gmail.com>
2021-06-08 22:55:48 -0500

    Los 10 ejercicios en una sola prueba que permite agregar cambiar eliminar

--- jquery/js/scrip.js
+++ jquery/js/scrip.js
@@ -9,10 +9,13 @@ $(document).ready(function () {});
 //SELECCION DE ID
 const contenedor = $(".container");
 const parrafo = $(".container p");
+const contenedor_p = $("#paragraph");
 const pintar = $("#pintar");
-const normalizar = $("#normalizar");
+const normalizar = $("#normalizar").prop("disabled", true);
 const desaparecer = $("#desaparecer");
 const agregar = $("#agregar");
+const div = $("<div></div>");
+let estado = 0;
 
 //Lo inserta al final
 
@@ -23,15 +26,63 @@ const agregar = $("#agregar");
 // $(contenedor).prepend("<h1>Este es un elemento</h1>");
 
 pintar.click(function (e) {
-  parrafo.addClass("text-danger display-4");
+  if (estado == 0) {
+    contenedor_p.addClass("text-danger display-4");
+    normalizar.prop("disabled", false);
+  } else {
+    div.addClass("text-danger display-4");
+    normalizar.prop("disabled", false);
+  }
 });
 
 normalizar.click(function (e) {
-  parrafo.removeClass("text-danger display-4");
+  console.log(estado);
+  if (estado == 0) {
+    contenedor_p.removeClass("text-danger display-4");
+    normalizar.prop("disabled", true);
+  } else if (estado == 2) {
+    div.removeClass("text-danger display-4");
+    normalizar.prop("disabled", true);
+  } else {
+    contenedor.prepend(div);
+    pintar.prop("disabled", false);
+    normalizar.prop("disabled", false);
+    agregar.prop("disabled", false);
+    desaparecer.prop("disabled", false);
+    estado = 2;
+    console.log(estado);
+  }
 });
 
-const nuevo = $("h1").html("Nuevo elemento");
-
 agregar.click(function (e) {
-  container.append(nuevo);
+  if (estado == 0) {
+    const nuevo = $("<p></p>").text(
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam quislaborum incidunt ab sint. Eius provident quasi fuga unde numquamOfficiis veritatis laudantium eius totam animi enim assumenda consectetur at."
+    );
+    contenedor_p.append(nuevo);
+  } else {
+    const nuevo = $("<p></p>").text(
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam quislaborum incidunt ab sint. Eius provident quasi fuga unde numquamOfficiis veritatis laudantium eius totam animi enim assumenda consectetur at."
+    );
+    div.append(nuevo);
+  }
+});
+
+desaparecer.click(function (e) {
+  if (estado == 0) {
+    contenedor_p.remove();
+    pintar.prop("disabled", true);
+    agregar.prop("disabled", true);
+    desaparecer.prop("disabled", true);
+    normalizar.prop("disabled", false);
+    estado = 1;
+    console.log(estado);
+  } else {
+    div.remove();
+    pintar.prop("disabled", true);
+    agregar.prop("disabled", true);
+    desaparecer.prop("disabled", true);
+    normalizar.prop("disabled", false);
+    estado = 1;
+  }
 });
